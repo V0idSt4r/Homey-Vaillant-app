0013d7a32ec5ca4ed484b290d5d90530d46d9acb
V0idSt4r <christoph.klopf@gmail.com>
2026-07-30 10:32:00 +0200

    reverted to cloud logging

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 const Homey = require('homey');
 const VaillantAuthentication = require('./lib/vaillant-authentication');
 const { ReauthenticationRequiredError } = require('./lib/vaillant-authentication');
-const VaillantApi = require('./lib/vaillant-api');
 const Logger = require('./lib/logger');
 
 module.exports = class MyApp extends Homey.App {
@@ -25,30 +24,6 @@ module.exports = class MyApp extends Homey.App {
       }
     } else {
       this.logger.info('No stored Vaillant session found. Skipping token renewal.');
-    }
-
-    if (this.homey.settings.get('loggingEnabled')) {
-      await this.logSystemInformation();
-    }
-  }
-
-  async logSystemInformation() {
-    this.api = new VaillantApi(this.homey.settings, this.logger, this.authentication);
-    try {
-      const devices = await this.api.getHeatingSystemsList();
-      if (!devices) {
-        return;
-      }
-
-      for (const device of devices) {
-        await this.api.getSystem(device.id);
-      }
-    } catch (error) {
-      if (error instanceof ReauthenticationRequiredError) {
-        this.logger.error('Stored Vaillant session expired during system information refresh.');
-      } else {
-        throw error;
-      }
     }
   }
 
